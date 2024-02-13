13167c9210dc61bf5726093675773bf797d71225
yoshikouki <yoshikouki@gmail.com>
2024-02-14 00:18:23 +0900

    Error with @hono/react-renderer

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
-import { Hono } from 'hono'
-import { renderer } from './renderer'
+import { reactRenderer } from "@hono/react-renderer";
+import { Hono } from 'hono';
 
 const app = new Hono()
 
-app.get('*', renderer)
+app.get(
+  "*",
+  reactRenderer(({ children }) => {
+    return (
+      <html>
+        <body>
+          <h1>React + Hono</h1>
+          <div>{children}</div>
+        </body>
+      </html>
+    );
+  })
+);
 
 app.get('/', (c) => {
   return c.render(<h1>Hello!</h1>)
